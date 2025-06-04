03ddaa0c1af199cca13f67713a6feff8e71c7b6f
Lootyfruty <lootyfruty@Lootyfrutys-MacBook-Air.local>
2025-06-04 19:04:25 +0300

    Add: Basic styles, icon, button

---
+++ front/src/App.jsx
@@ -0,0 +1,23 @@
+import { Button } from "./components/Button";
+
+const App = () => {
+  return (
+    <div className="bg-mono-8">
+      <h1 class="text-3xl bg-amber-400">dsfdsfsdffde</h1>
+      <Button type="primary" size="small" icon="star">
+        Excellent
+      </Button>
+      <Button type="secondary" size="main" icon="star">
+        Remarkable
+      </Button>
+      <Button type="positive" size="large" icon="star">
+        Fantastic
+      </Button>
+      <Button type="positive" size="xsize" icon="star">
+        Outstanding
+      </Button>
+    </div>
+  );
+};
+
+export { App };
---
+++ front/src/components/Button.jsx
@@ -0,0 +1,29 @@
+import { Icon } from "./Icon";
+
+const Button = ({ children, type, size, icon }) => {
+  const buttonType = {
+    primary: ["bg-mono-100 text-mono-8", "gradient"],
+    secondary: ["bg-avocado-50 text-mono-92", "fill-emerald-200"],
+    tertary: ["bg-mono-0 text-mono-92", "gradient"],
+    ghost: ["bg-mono-4 text-mono-84", "fill-mono-25"],
+    positive: ["bg-emerald-400 text-emerald-900", "gradient"],
+    disable: ["text-mono-25", "fill-mono-25"],
+    attention: ["text-vermel-500", "fill-vermel-500"],
+  };
+
+  const buttonSize = {
+    small: "font-base text-default-bold flex rounded-full w-fit px-2 py-1",
+    main: "font-base text-large-bold flex rounded-full w-fit px-2 py-2",
+    large: "font-base text-body flex rounded-3xl w-fit px-4 py-4",
+    xsize: "font-base text-body flex rounded-3xl w-fit px-6 py-5",
+  };
+
+  return (
+    <div className={`${buttonType[type][0]} ${buttonSize[size]}`}>
+      <Icon type={icon} size="24" style={buttonType[type][1]} />
+      <div className="mx-2">{children}</div>
+    </div>
+  );
+};
+
+export { Button };
